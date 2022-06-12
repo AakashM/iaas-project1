0f2f7c8abb1d66f4136151d12719fa6fc4a184dc
Aakash Murari <aakash.murari@gmail.com>
2022-06-12 13:08:50 -0700

    Added myfile param to controller

--- web-tier/src/main/java/com/iaas/project1/webtier/ClassifyController.java
+++ web-tier/src/main/java/com/iaas/project1/webtier/ClassifyController.java
@@ -1,14 +1,16 @@
 package com.iaas.project1.webtier;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ClassifyController {
 
     @RequestMapping("/")
-    public String classify() {
-        System.out.println("Received message");
+    public String classify(@RequestParam("myfile") MultipartFile file) {
+        System.out.println("Received file: " + file.getOriginalFilename());
         return "Hello World from Spring Boot";
     }
 }
